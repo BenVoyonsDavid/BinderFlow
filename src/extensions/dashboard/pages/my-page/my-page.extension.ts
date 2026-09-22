0f2c1e543c331a206b2ca77c99f296c64121923a
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-22 15:14:13 -0400

    chore: migrate BinderFlow to Wix App scaffold

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'b6d947c7-b6bf-4284-8738-a1c7837f9548',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+  fullPage: false,
+});
